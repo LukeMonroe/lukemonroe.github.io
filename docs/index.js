69d4cdcde839e8799d8c9e03523625d0f9cc083c
Luke Monroe <Luke.monroe.git@gmail.com>
2022-12-08 00:58:38 -0500

    Experimenting with HTML and CSS.

--- docs/index.js
+++ docs/index.js
@@ -5,3 +5,19 @@ function openTab(event, tabName) {
     }
     document.getElementById(tabName).style.display = "flex";
 }
+
+function email() {
+    emailjs.init("I4v5nKkBfKnOtb346");
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm("contact_service", "contact_form", this)
+            .then(function () {
+                console.log("SUCCESS!");
+            }, function (error) {
+                console.log("FAILED...", error);
+            });
+    });
+}
