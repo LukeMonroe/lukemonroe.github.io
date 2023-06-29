ef4ec199d53265837d6df1a4d25a577ab6608385
Luke Monroe <Luke.monroe.git@gmail.com>
2023-06-29 16:57:11 -0400

    Experimenting with HTML, CSS, and JS.

--- game/index.js
+++ game/index.js
@@ -238,6 +238,7 @@ function resizeCanvas () {
 }
 
 function start () {
+  document.body.requestFullscreen()
   startButtons.style.visibility = HIDDEN
   scale = canvas.width / CANVAS_MAX_WIDTH
   player = Player.create(canvas, scale)
