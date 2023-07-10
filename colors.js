228ca9bd2bccd7f2c72b47c6e227c17b0b9fc50b
Luke Monroe <Luke.monroe.git@gmail.com>
2023-07-10 04:58:35 -0400

    Experimenting with HTML, CSS, and JS.

--- colors.js
+++ colors.js
@@ -1,22 +1,24 @@
 class Colors {
+  static randomColor () {
+    const hsl = Colors.randomHSL()
+    const rgb = Colors.hslToRGB(hsl)
+    const grayscale = Math.round((0.2126 * rgb.r) + (0.7152 * rgb.g) + (0.0722 * rgb.b))
+
+    return { hsl, rgb, grayscale }
+  }
+
   static randomHSL () {
     const h = Math.round(Math.random() * 359)
     const s = Math.round(Math.random() * 100)
     const l = Math.round(Math.random() * 100)
 
-    const rgb = Colors.hslToRGB(h, s, l)
-    const grayscale = Math.round((0.2126 * rgb.r) + (0.7152 * rgb.g) + (0.0722 * rgb.b))
-
-    console.log(`hsl(${h}, ${s}%, ${l}%)`)
-    console.log(`rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`)
-    console.log(grayscale)
-
-    return { h, s, l, rgb, grayscale }
+    return { h, s, l }
   }
 
-  static hslToRGB (h, s, l) {
-    s /= 100
-    l /= 100
+  static hslToRGB (hsl) {
+    const h = hsl.h
+    const s = hsl.s / 100
+    const l = hsl.l / 100
 
     const c = (1 - Math.abs((2 * l) - 1)) * s
     const x = c * (1 - Math.abs(((h / 60) % 2) - 1))
@@ -45,6 +47,14 @@ class Colors {
     b = Math.round((b + m) * 255)
 
     return { r, g, b }
+  }
+
+  static formatHSL (color) {
+    return `hsl(${color.hsl.h}, ${color.hsl.s}%, ${color.hsl.l}%)`
+  }
+
+  static formatRGB (color) {
+    return `rgb(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`
   }
 }
 
--- recipes/recipes-themes.js
+++ recipes/recipes-themes.js
@@ -3,14 +3,14 @@ import { Colors } from '../colors.js'
 
 class RecipesThemes extends Themes {
   changeTheme (theme) {
-    let hsl = Colors.randomHSL()
+    let color = Colors.randomColor()
     if (this.light(theme)) {
-      while (hsl.grayscale > 150) { hsl = Colors.randomHSL() }
+      while (color.grayscale > 150) { color = Colors.randomColor() }
     } else {
-      while (hsl.grayscale <= 150) { hsl = Colors.randomHSL() }
+      while (color.grayscale <= 150) { color = Colors.randomColor() }
     }
 
-    document.documentElement.style.setProperty('--random-color', `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`)
+    document.documentElement.style.setProperty('--random-color', Colors.formatHSL(color))
     document.documentElement.style.setProperty('--background-color', this.backgroundColor(theme))
     document.documentElement.style.setProperty('--text-color', this.textColor(theme))
   }
