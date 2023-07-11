5a49b79217e8a63174076138d0b7ce3c7adcdea7
Luke Monroe <Luke.monroe.git@gmail.com>
2023-07-11 04:26:26 -0400

    Experimenting with HTML, CSS, and JS.

--- prints/index.js
+++ prints/index.js
@@ -3,7 +3,14 @@ import { PrintsThemes } from './prints-themes.js'
 const printsThemes = new PrintsThemes()
 printsThemes.setTheme()
 
+let fullscreen = false
 const image = document.getElementById('image')
 image.addEventListener('click', () => {
-  image.requestFullscreen()
+  if (fullscreen) {
+    document.exitFullscreen()
+    fullscreen = false
+  } else {
+    image.requestFullscreen()
+    fullscreen = true
+  }
 })
