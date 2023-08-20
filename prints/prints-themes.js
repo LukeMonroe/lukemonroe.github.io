8f65132f047db2cf47ab3a017ed6b8e42f5d23ac
Luke Monroe <Luke.monroe.git@gmail.com>
2023-08-20 13:28:49 -0400

    Experimenting with HTML, CSS, and JS.

--- prints/prints-themes.js
+++ prints/prints-themes.js
@@ -1,26 +1,13 @@
 import { Themes } from '../themes.js'
-import { Colors } from '../colors.js'
 
 class PrintsThemes extends Themes {
   changeTheme (theme) {
-    let randomColor01 = Colors.random()
-    let randomColor02 = Colors.random()
     if (this.light(theme)) {
-      while (randomColor01.grayscale > 150) { randomColor01 = Colors.random() }
-      while (randomColor02.grayscale > 150) { randomColor02 = Colors.random() }
-
       document.documentElement.style.setProperty('--background-color', this.backgroundColor.formattedHex)
-      document.documentElement.style.setProperty('--color', this.backgroundColor.formattedHex)
-      document.documentElement.style.setProperty('--gradient-start-color', randomColor01.formattedHex)
-      document.documentElement.style.setProperty('--gradient-end-color', randomColor02.formattedHex)
-    } else {
-      while (randomColor01.grayscale <= 150) { randomColor01 = Colors.random() }
-      while (randomColor02.grayscale <= 150) { randomColor02 = Colors.random() }
-
-      document.documentElement.style.setProperty('--background-color', this.color.formattedHex)
       document.documentElement.style.setProperty('--color', this.color.formattedHex)
-      document.documentElement.style.setProperty('--gradient-start-color', randomColor01.formattedHex)
-      document.documentElement.style.setProperty('--gradient-end-color', randomColor02.formattedHex)
+    } else {
+      document.documentElement.style.setProperty('--background-color', this.color.formattedHex)
+      document.documentElement.style.setProperty('--color', this.backgroundColor.formattedHex)
     }
   }
 }
