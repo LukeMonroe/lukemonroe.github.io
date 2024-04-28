84923eca68fc0006a327a882f04e3869e48e7f72
Luke Monroe <Luke.monroe.git@gmail.com>
2024-04-28 18:07:54 -0400

    Experimenting with HTML, CSS, and JS.

--- random/create-a-question/index.js
+++ random/create-a-question/index.js
@@ -10,41 +10,41 @@ const questionColumn = createDivInnerColumn()
 
 const inputQuestionRow = createDivInnerRow()
 inputQuestionRow.appendChild(inputQuestion)
-inputQuestionRow.appendChild(createButtonPlus())
+inputQuestionRow.appendChild(createButtonEnter())
 
 const inputQuestionColumn = createDivInnerColumn()
-inputQuestionColumn.appendChild(createH3("Type in the question you wish to ask someone."))
+inputQuestionColumn.appendChild(createH2("Enter the yes or no question you wish to ask."))
 inputQuestionColumn.appendChild(inputQuestionRow)
 
 const outerColumn = document.getElementById('outer-column')
 outerColumn.appendChild(inputQuestionColumn)
 outerColumn.appendChild(questionColumn)
 
-function createDivInnerColumn () {
+function createDivInnerColumn() {
   return createDiv('inner-column')
 }
 
-function createDivInnerRow () {
+function createDivInnerRow() {
   return createDiv('inner-row')
 }
 
-function createInputQuestion () {
-  const inputToDo = document.createElement('input')
-  inputToDo.className = 'to-do'
-  inputToDo.type = 'text'
-  inputToDo.addEventListener('keydown', event => {
+function createInputQuestion() {
+  const inputQuestion = document.createElement('input')
+  inputQuestion.className = 'question'
+  inputQuestion.type = 'text'
+  inputQuestion.addEventListener('keydown', event => {
     if (event.key === 'Enter') {
       event.preventDefault()
       addQuestion()
     }
   })
 
-  return inputToDo
+  return inputQuestion
 }
 
-function createButtonYes () {
+function createButtonYes() {
   const button = document.createElement('button')
-  button.innerText = 'Yes :)'
+  button.appendChild(createH4('Yes'))
   button.addEventListener('click', () => {
     questionColumn.replaceChildren()
     questionColumn.appendChild(createDivQuestionColumn("You're Awesome!"))
@@ -53,88 +53,93 @@ function createButtonYes () {
   return button
 }
 
-function createButtonNo () {
+function createButtonNo() {
   const button = document.createElement('button')
-  button.innerText = 'No :('
-  button.addEventListener('click', () => {
-    questionColumn.replaceChildren()
-    questionColumn.appendChild(createDivQuestionColumn("Your perseverance is noted."))
+  button.appendChild(createH4('No'))
+  button.appendChild(x)
+  button.appendChild(y)
+  button.addEventListener('touchstart', () => {
+    handleNo(button)
   })
   button.addEventListener('mouseover', () => {
-    button.style.position = 'absolute'
-    button.style.top = `${rangeRandom(Number(50), Number(window.innerHeight - 50))}px`
-    button.style.left = `${rangeRandom(Number(50), Number(window.innerWidth - 50))}px`
-    button.style.rotate = `${rangeRandom(Number(0), Number(360))}deg`
-    const size = rangeRandom(Number(20), Number(200))
-    button.style.width = `${size}px`
-    button.style.height = `${size}px`
+    handleNo(button)
   })
 
   return button
 }
 
-function rangeRandom (floor, ceil) {
-  const number = Math.ceil(limitRandom(ceil))
-  return number < floor ? floor : number
+var clicks = 0
+var addedEventListener = false
+var x = createH4('X')
+var y = createH4('Y')
+function handleNo(button) {
+  if (clicks >= 20) {
+    if (!addedEventListener) {
+      button.addEventListener('click', () => {
+        questionColumn.replaceChildren()
+        questionColumn.appendChild(createDivQuestionColumn("Your perseverance is noted."))
+      })
+      button.style.position = 'relative'
+      button.style.top = '0px'
+      button.style.left = '0px'
+      button.style.rotate = '0deg'
+      button.style.transition = 'None'
+      addedEventListener = true
+    }
+  } else {
+    button.style.position = 'absolute'
+    button.style.top = `${rangeRandom(Number(50), Number(window.innerHeight - 50))}px`
+    y.innerText = button.style.top
+    button.style.left = `${rangeRandom(Number(50), Number(document.body.clientWidth - 50))}px`
+    x.innerText = button.style.left
+    button.style.rotate = `${rangeRandom(Number(0), Number(360))}deg`
+    button.style.transition = '1s'
+    clicks++
+  }
 }
 
-function limitRandom (limit) {
-  return random() * limit
+function createButtonEnter() {
+  const buttonEnter = document.createElement('button')
+  buttonEnter.appendChild(createH4('Enter'))
+  buttonEnter.addEventListener('click', addQuestion)
+
+  return buttonEnter
 }
 
-function random () {
-  return Math.random()
-}
-
-function createButtonPlus () {
-  const buttonPlus = document.createElement('button')
-  buttonPlus.className = 'plus'
-  buttonPlus.addEventListener('click', addQuestion)
-
-  return buttonPlus
-}
-
-function createDivQuestionColumn (toDo) {
+function createDivQuestionColumn(question) {
   const divButtonRow = createDivInnerRow()
   divButtonRow.appendChild(createButtonYes())
   divButtonRow.appendChild(createButtonNo())
-  
+
   const divQuestionColumn = createDivInnerColumn()
-  divQuestionColumn.appendChild(createDivQuestion(toDo))
+  divQuestionColumn.appendChild(createDivQuestion(question))
   divQuestionColumn.appendChild(divButtonRow)
 
   return divQuestionColumn
 }
 
-function createDivQuestion (toDo) {
-  const divToDo = createDiv('to-do')
-  divToDo.appendChild(createH2(toDo))
+function createDivQuestion(question) {
+  const divQuestion = createDiv('question')
+  divQuestion.appendChild(createH2(question))
 
-  return divToDo
+  return divQuestion
 }
 
-function createH2 (innerText) {
+function createH2(innerText) {
   const h2 = document.createElement('h2')
   h2.innerText = innerText
 
   return h2
 }
 
-function createH3 (innerText) {
-  const h3 = document.createElement('h2')
-  h3.innerText = innerText
-
-  return h3
-}
-
-function createH4 (innerText) {
+function createH4(innerText) {
   const h4 = document.createElement('h4')
   h4.innerText = innerText
 
   return h4
 }
 
-function createDiv (className) {
+function createDiv(className) {
   const div = document.createElement('div')
   if (className !== null) {
     div.className = className
@@ -143,19 +148,32 @@ function createDiv (className) {
   return div
 }
 
-function addQuestion () {
+function addQuestion() {
   if (inputQuestion.value !== null) {
-    let toDo = inputQuestion.value.trim()
-    if (toDo.length > 0) {
-      toDo = `${toDo.slice(0, 1).toUpperCase()}${toDo.slice(1)}`
-      if (toDo.slice(-1) != "?") {
-        toDo = `${toDo}?`
+    let question = inputQuestion.value.trim()
+    if (question.length > 0) {
+      question = `${question.slice(0, 1).toUpperCase()}${question.slice(1)}`
+      if (question.slice(-1) != "?") {
+        question = `${question}?`
       }
       questionColumn.replaceChildren()
-      questionColumn.appendChild(createDivQuestionColumn(toDo))
+      questionColumn.appendChild(createDivQuestionColumn(question))
+
+      inputQuestion.value = null
+      inputQuestionColumn.style.display = "None"
     }
   }
+}
 
-  inputQuestion.value = null
-  inputQuestionColumn.style.visibility = "hidden"
+function rangeRandom(floor, ceil) {
+  const number = Math.ceil(limitRandom(ceil))
+  return number < floor ? floor : number
+}
+
+function limitRandom(limit) {
+  return random() * limit
+}
+
+function random() {
+  return Math.random()
 }
