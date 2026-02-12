e5dfeeaf71aa8c0cdf7fc31b8b22368624ac7cba
Luke Monroe <Luke.monroe.git@gmail.com>
2026-02-12 02:14:03 -0500

    Experimenting with HTML, CSS, and JS.

--- gradient-picker-page-new.js
+++ gradient-picker-page-new.js
@@ -1,6 +1,10 @@
 import { ColorPickerPage } from './color-picker-page.js'
 import { Colors } from './colors.js'
-import { createH1 } from './text.js'
+import { createDivGradientIconHeart, isGradientLiked } from './favorites.js'
+import { getBackgroundImage } from './images.js'
+import { createDivTooltip } from './tooltips.js'
+import { createH1, createDivColorText } from './text.js'
+import { createDivGradientIconFullscreen } from './fullscreen.js'
 
 class GradientPickerPageNew extends ColorPickerPage {
 
@@ -20,11 +24,8 @@ class GradientPickerPageNew extends ColorPickerPage {
       const divColor02 = this.createDivColor(Colors.hue(color, 90), false)
       divColor02.style.display = 'none'
 
-      const divGradient = this.createDivColor(color, false)
-      const type = 'linear'
-      const value = '180deg'
-      const position = '0%'
-      divGradient.style.background = `${type}-gradient(${value}, ${color.formattedHex} ${position}, ${Colors.hue(color, 90).formattedHex})`
+      const divGradient = this.createDivGradient(color, Colors.hue(color, 90), divColor01, divColor02)
+      divGradient.style.display = 'flex'
 
       const divColorRow = this.createDivColorRowNew()
       divColorRow.style.flex = '1 1 0'
@@ -33,9 +34,6 @@ class GradientPickerPageNew extends ColorPickerPage {
       divColorRow.appendChild(divGradient)
       divColorRow.addEventListener('mouseenter', () => {
         divColorRow.style.flex = 'auto'
-        divColor01.style.display = 'flex'
-        divColor02.style.display = 'flex'
-        divGradient.style.display = 'none'
       })
       divColorRow.addEventListener('mouseleave', () => {
         divColorRow.style.flex = '1 1 0'
@@ -45,9 +43,6 @@ class GradientPickerPageNew extends ColorPickerPage {
       })
       divColorRow.addEventListener('click', () => {
         divColorRow.style.flex = 'auto'
-        divColor01.style.display = 'flex'
-        divColor02.style.display = 'flex'
-        divGradient.style.display = 'none'
       })
       row.appendChild(divColorRow)
     })
@@ -84,6 +79,94 @@ class GradientPickerPageNew extends ColorPickerPage {
     for (let index = (colors.length > 8 ? 8 : colors.length); index < 16; index++) {
       localStorage.removeItem(`historyColorNew${index}`)
     }
+  }
+
+  createDivGradientIconCheckmark(gradient) {
+    const divColorIcon = document.createElement('div')
+    divColorIcon.className = 'color-icon'
+    divColorIcon.style.backgroundImage = getBackgroundImage(gradient[1], 'checkmark')
+    divColorIcon.style.bottom = '10px'
+    divColorIcon.style.right = '10px'
+    createDivTooltip(divColorIcon, 'load')
+    divColorIcon.addEventListener('click', () => {
+      this.updatePage(gradient[0])
+    })
+
+    return divColorIcon
+  }
+
+  createDivColorIconCornerTriangle(gradient, divColors, divGradient) {
+    const divColorIcon = document.createElement('div')
+    divColorIcon.className = 'color-icon'
+    divColorIcon.style.backgroundImage = getBackgroundImage(gradient[0], 'corner-triangle')
+    divColorIcon.style.bottom = '10px'
+    divColorIcon.style.left = '10px'
+    createDivTooltip(divColorIcon, 'show colors')
+    divColorIcon.addEventListener('click', () => {
+      divColors[0].style.display = 'flex'
+      divGradient.style.display = 'none'
+      divColors[1].style.display = 'flex'
+    })
+
+    return divColorIcon
+  }
+
+  createDivGradient(color01, color02, divColor01, divColor02) {
+    const divMarker = document.createElement('div')
+    divMarker.className = 'marker'
+    divMarker.style.backgroundColor = color01.formattedText
+
+    const type = 'linear'
+    const value = '180deg'
+    const position = '0%'
+
+    const likeGradient = createDivGradientIconHeart([color01, color02])
+    const divGradient = document.createElement('div')
+    divGradient.className = 'color'
+    divGradient.style.backgroundColor = color01.formattedHex
+    divGradient.style.color = color01.formattedText
+    divGradient.style.background = `${type}-gradient(${value}, ${color01.formattedHex} ${position}, ${color02.formattedHex})`
+    divGradient.appendChild(createDivColorText(`${type}-gradient(${value}, ${color01.formattedHex} ${position}, ${color02.formattedHex})`))
+    divGradient.appendChild(likeGradient)
+    divGradient.appendChild(createDivGradientIconFullscreen([color01, color02], type, value, position))
+    divGradient.appendChild(this.createDivGradientIconCheckmark([color01, color02]))
+    divGradient.appendChild(this.createDivColorIconCornerTriangle([color01, color02], [divColor01, divColor02], divGradient))
+    if (Colors.equal(color01, this.colorPicked)) {
+      divGradient.appendChild(divMarker)
+    }
+
+    divGradient.style.flex = '1 1 0'
+    divGradient.addEventListener('mouseenter', () => {
+      const children = divGradient.children
+      for (let index = 0; index < children.length; index++) {
+        children[index].style.display = 'block'
+      }
+      divGradient.style.flex = 'auto'
+      divGradient.style.boxShadow = `2px 2px ${divGradient.style.color} inset, -2px -2px ${divGradient.style.color} inset`
+      divMarker.style.display = 'none'
+      likeGradient.style.backgroundImage = isGradientLiked([color01, color02]) ? getBackgroundImage(color01, 'heart-filled') : getBackgroundImage(color01, 'heart-empty')
+    })
+    divGradient.addEventListener('mouseleave', () => {
+      const children = divGradient.children
+      for (let index = 0; index < children.length; index++) {
+        children[index].style.display = 'none'
+      }
+      divGradient.style.flex = '1 1 0'
+      divGradient.style.boxShadow = 'none'
+      divMarker.style.display = 'block'
+    })
+    divGradient.addEventListener('click', () => {
+      const children = divGradient.children
+      for (let index = 0; index < children.length; index++) {
+        children[index].style.display = 'block'
+      }
+      divGradient.style.flex = 'auto'
+      divGradient.style.boxShadow = `2px 2px ${divGradient.style.color} inset, -2px -2px ${divGradient.style.color} inset`
+      divMarker.style.display = 'none'
+      likeGradient.style.backgroundImage = isGradientLiked([color01, color02]) ? getBackgroundImage(color01, 'heart-filled') : getBackgroundImage(color01, 'heart-empty')
+    })
+
+    return divGradient
   }
 }
 
