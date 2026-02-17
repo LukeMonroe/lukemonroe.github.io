757310fa0a411c8250b37d2956126a38418bbdce
Luke Monroe <Luke.monroe.git@gmail.com>
2026-02-17 00:22:42 -0500

    Experimenting with HTML, CSS, and JS.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { ColorPickerPage } from './color-picker-page.js'
 import { GradientPickerPage } from './gradient-picker-page.js'
-import { GradientPickerPageNew } from './gradient-picker-page-new.js'
 import { FavoritesPage } from './favorites-page.js'
 import { ColorPicker } from './color-picker.js'
 import { ImagePicker } from './image-picker.js'
@@ -15,12 +14,10 @@ const imagePicker = new ImagePicker()
 
 const colorPickerPage = new ColorPickerPage(buttonNavigation, colorPicker, imagePicker)
 const gradientPickerPage = new GradientPickerPage(buttonNavigation, colorPicker)
-const gradientPickerPageNew = new GradientPickerPageNew(buttonNavigation, colorPicker, imagePicker)
 const favoritesPage = new FavoritesPage(buttonNavigation, colorPicker)
 
 sideNavigation.colorPickerPage = colorPickerPage
 sideNavigation.gradientPickerPage = gradientPickerPage
-sideNavigation.gradientPickerPageNew = gradientPickerPageNew
 sideNavigation.favoritesPage = favoritesPage
 
 const tool = localStorage.getItem('tool')
@@ -28,8 +25,6 @@ if (tool === null || tool === 'colorPicker') {
   colorPickerPage.createPage()
 } else if (tool === 'gradientPicker') {
   gradientPickerPage.createPage()
-} else if (tool === 'gradientPickerNew') {
-  gradientPickerPageNew.createPage()
 } else {
   favoritesPage.createPage()
 }
--- navigation.js
+++ navigation.js
@@ -5,7 +5,6 @@ class SideNavigation {
   constructor() {
     this.colorPickerPage = null
     this.gradientPickerPage = null
-    this.gradientPickerPageNew = null
     this.favoritesPage = null
     this.themes = new Themes()
     this.themes.setTheme()
@@ -41,15 +40,6 @@ class SideNavigation {
       }
     })
 
-    const aGradientsNew = this.createA('javascript:void(0);', 'Gradient Picker New')
-    aGradientsNew.addEventListener('click', () => {
-      sideNavigation.style.width = '0px'
-      const tool = localStorage.getItem('tool')
-      if (tool !== 'gradientPickerNew') {
-        this.gradientPickerPageNew.createPage()
-      }
-    })
-
     const aFavorites = this.createA('javascript:void(0);', 'Favorites')
     aFavorites.addEventListener('click', () => {
       sideNavigation.style.width = '0px'
@@ -60,13 +50,12 @@ class SideNavigation {
     })
 
     const selectTheme = this.themes.createSelectTheme(true)
-    selectTheme.style.width = '260px'
+    selectTheme.style.width = '240px'
     selectTheme.style.margin = '30px 0px 0px 30px'
 
     sideNavigation.appendChild(selectTheme)
     sideNavigation.appendChild(aColors)
     sideNavigation.appendChild(aGradients)
-    sideNavigation.appendChild(aGradientsNew)
     sideNavigation.appendChild(aFavorites)
     document.body.appendChild(sideNavigation)
 
@@ -76,15 +65,15 @@ class SideNavigation {
     buttonNavigation.style.fontSize = '24px'
     buttonNavigation.addEventListener('click', event => {
       event.stopPropagation()
-      if (sideNavigation.style.width === '320px') {
+      if (sideNavigation.style.width === '300px') {
         sideNavigation.style.width = '0px'
       } else {
-        sideNavigation.style.width = '320px'
+        sideNavigation.style.width = '300px'
       }
     })
 
     document.addEventListener('click', event => {
-      if (sideNavigation.style.width === '320px') {
+      if (sideNavigation.style.width === '300px') {
         if (!sideNavigation.contains(event.target)) {
           sideNavigation.style.width = '0px'
         }
